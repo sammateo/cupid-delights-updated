376715dfe0d9d9c24336952586968ceff5a1fbd2
Mateo Sam <sammateo4@gmail.com>
2023-12-25 18:36:27 -0400

    total

--- components/cart/Cart.tsx
+++ components/cart/Cart.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { createClient } from "@supabase/supabase-js";
 import Image from "next/image";
+import Link from "next/link";
 import { useState, useEffect, ChangeEvent } from "react";
 var formatter = new Intl.NumberFormat("en-IN", {
 	style: "currency",
@@ -58,9 +59,9 @@ function Cart({ setShowCart }: any) {
 	}, []);
 
 	return (
-		<div className="text-primary_fg backdrop-filter backdrop-blur-sm bg-opacity-10  border border-primary_fg mx-auto min-w-[350px] max-w-[700px] text-center rounded shadow-lg shadow-secondary_bg px-2 py-4 mb-2 fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-			<div className="flex justify-between items-center">
-				<p className="text-secondary_fg text-xl font-medium">Cart</p>
+		<div className="text-primary_fg backdrop-filter backdrop-blur-sm bg-opacity-10 border border-primary_fg mx-auto min-w-[350px] max-w-[700px] text-center rounded shadow-lg shadow-secondary_bg px-2 py-4 mb-2 fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+			<div className="flex justify-between items-center text-secondary_fg">
+				<p className=" text-xl font-medium">Cart</p>
 				<IoClose
 					className="text-2xl hover:cursor-pointer"
 					onClick={() => {
@@ -135,7 +136,7 @@ function Cart({ setShowCart }: any) {
 							<p className="text-secondary_fg text-xl">Shipping</p>
 
 							<div>
-								<div className="flex justify-between bg-primary_bg py-2 px-4 text-left rounded border border-secondary_fg">
+								<div className="flex justify-between items-center bg-primary_bg py-2 px-4 text-left rounded border border-secondary_fg">
 									<input
 										type="radio"
 										name="shippingtype"
@@ -159,7 +160,7 @@ function Cart({ setShowCart }: any) {
 										<p>minimum 6 rotis</p>
 									)}
 								</div>
-								<div className="flex justify-between bg-primary_bg py-2 px-4 text-left rounded border border-secondary_fg my-2">
+								<div className="flex justify-between items-center bg-primary_bg py-2 px-4 text-left rounded border border-secondary_fg my-2">
 									<input
 										type="radio"
 										name="shippingtype"
@@ -211,16 +212,45 @@ function Cart({ setShowCart }: any) {
 									)}
 								</p>
 							</div>
+							<div className="flex justify-between border-t-2 border-primary_fg">
+								<p className="">Total</p>
+								<p>
+									{getQuantity() >= 6 && shipping ? (
+										shipping.map((ship: any) =>
+											getQuantity() >= ship.lower_range &&
+											getQuantity() <= ship.upper_range ? (
+												<p>
+													{shippingType == "express"
+														? formatter.format(ship.express + getSubTotal())
+														: formatter.format(ship.next_day + getSubTotal())}
+												</p>
+											) : (
+												<></>
+											)
+										)
+									) : (
+										<p>minimum 6 rotis</p>
+									)}
+								</p>
+							</div>
 						</div>
 						<div className="">
-							{getQuantity() >= 6 ? (
-								<button className="bg-primary_fg text-primary_bg py-1 px-5 mt-2 text-lg rounded w-8/12">
-									Checkout
-								</button>
-							) : (
+							{getQuantity() < 6 ? (
 								<p className="text-secondary_fg bg-primary_bg py-1 px-5 mt-2 mx-auto text-lg rounded w-8/12 border border-secondary_fg">
 									minimum 6 rotis
 								</p>
+							) : getQuantity() > 40 ? (
+								<div>
+									<p className="text-secondary_fg bg-primary_bg py-1 px-5 mt-2 mx-auto rounded w-8/12 border border-secondary_fg">
+										Contact us for orders over 40 rotis
+									</p>
+								</div>
+							) : (
+								<Link href="/checkout">
+									<button className="bg-primary_fg text-primary_bg py-1 px-5 mt-2 text-lg rounded w-8/12">
+										Checkout
+									</button>
+								</Link>
 							)}
 						</div>
 					</div>
--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -5,11 +5,11 @@ export default function Navbar() {
 		<div className="flex flex-wrap justify-center mt-2 mx-auto gap-x-20">
 			<p className="text-secondary_fg font-bold px-4 py-2 ">Cupid Delights</p>
 			<div className="text-primary_fg flex flex-wrap justify-evenly gap-10 shadow-md shadow-secondary_bg px-4 py-2 rounded-full font-medium">
-				<Link href="#">
+				<Link href="/">
 					<p>Home</p>
 				</Link>
 
-				<Link href="#menu">
+				<Link href="/#menu">
 					<p>Menu</p>
 				</Link>
 
